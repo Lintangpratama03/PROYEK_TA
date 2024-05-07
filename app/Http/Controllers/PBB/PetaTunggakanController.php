839c3722e4f13fac7575ecf64c21821616f466bd
2131730057@student.polinema.ac.id <2131730057@student.polinema.ac.id>
2024-05-07 09:50:54 +0700

    tambahan

--- app/Http/Controllers/PBB/PetaTunggakanController.php
+++ app/Http/Controllers/PBB/PetaTunggakanController.php
@@ -10,11 +10,77 @@ class PetaTunggakanController extends Controller
 {
     public function index()
     {
-        $koordinat = DB::connection("pgsql_pbb")
-            ->table("data.koordinat")->get();
-        $tunggakan = DB::connection("pgsql_pbb")
-            ->table("data.detail_tunggakan")->get();
-        return view('admin.pbb.peta.peta', compact('koordinat', 'tunggakan'));
+        $wilayah = DB::connection('pgsql_pbb')
+            ->table('data.wilayah')
+            ->select('kecamatan', 'kelurahan', DB::raw('ST_AsGeoJSON(geometry) as geometry'))
+            ->get();
+
+        $tunggakan = DB::connection('pgsql_pbb')
+            ->table('data.detail_tunggakan')
+            ->get();
+
+        // Inisialisasi variabel $kelurahanData
+        $kelurahanData = [];
+        foreach ($tunggakan as $item) {
+            $kecamatan = $item->kecamatan;
+            $kelurahan = $item->kelurahan;
+            $level = $item->level;
+            if (!isset($kelurahanData[$kecamatan][$kelurahan])) {
+                $kelurahanData[$kecamatan][$kelurahan] = [
+                    'BERAT' => ['jumlah' => 0, 'nominal' => 0],
+                    'SEDANG' => ['jumlah' => 0, 'nominal' => 0],
+                    'RINGAN' => ['jumlah' => 0, 'nominal' => 0],
+                ];
+            }
+            $kelurahanData[$kecamatan][$kelurahan][$level]['jumlah'] += $item->jumlah_tunggakan;
+            $kelurahanData[$kecamatan][$kelurahan][$level]['nominal'] += $item->nominal_tunggakan;
+        }
+
+        // Inisialisasi variabel $sortedData
+        $sortedData = [];
+        foreach ($kelurahanData as $kecamatan => $kelurahan) {
+            foreach ($kelurahan as $nama => $data) {
+                $jumlahTunggakan = $data['BERAT']['jumlah'] + $data['SEDANG']['jumlah'] + $data['RINGAN']['jumlah'];
+                $nominalTunggakan = $data['BERAT']['nominal'] + $data['SEDANG']['nominal'] + $data['RINGAN']['nominal'];
+                $sortedData[] = [
+                    'kecamatan' => $kecamatan,
+                    'kelurahan' => $nama,
+                    'jumlah_tunggakan' => $jumlahTunggakan,
+                    'nominal_tunggakan' => $nominalTunggakan,
+                ];
+            }
+        }
+
+        usort($sortedData, function ($a, $b) {
+            if ($a['jumlah_tunggakan'] == $b['jumlah_tunggakan']) {
+                return $a['nominal_tunggakan'] > $b['nominal_tunggakan'] ? 1 : -1;
+            }
+            return $a['jumlah_tunggakan'] > $b['jumlah_tunggakan'] ? -1 : 1;
+        });
+
+        $formattedWilayah = $wilayah->map(function ($item) use ($kelurahanData) {
+            $kecamatan = $item->kecamatan;
+            $kelurahan = $item->kelurahan;
+            $tunggakanData = $kelurahanData[$kecamatan][$kelurahan] ?? [
+                'BERAT' => ['jumlah' => 0, 'nominal' => 0],
+                'SEDANG' => ['jumlah' => 0, 'nominal' => 0],
+                'RINGAN' => ['jumlah' => 0, 'nominal' => 0]
+            ];
+            $geometry = $item->geometry;
+
+            return [
+                'kecamatan' => $kecamatan,
+                'kelurahan' => $kelurahan,
+                'geometry' => $geometry,
+                'tunggakanData' => $tunggakanData,
+                'jumlahTunggakan' => array_sum(array_column($tunggakanData, 'jumlah')) // Total jumlah tunggakan
+            ];
+        });
+
+
+        $sortedData = collect($sortedData);
+        // dd($formattedWilayah);
+        return view('admin.pbb.peta.peta', compact('formattedWilayah', 'sortedData'));
     }
 
     public function create()
@@ -29,11 +95,9 @@ class PetaTunggakanController extends Controller
     {
     }
 
-
     public function edit($id)
     {
     }
-
 
     public function update(Request $request, $id)
     {
--- resources/views/admin/layout/main.blade.php
+++ resources/views/admin/layout/main.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" style="--theme-deafult: #d97f3d; --theme-secondary: #f25f4c;">
-  <head>
+
+<head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,52 +11,60 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <link rel="icon" href="{{asset("assets")}}/images/favicon.png" type="image/x-icon">
-    <link rel="shortcut icon" href="{{asset("assets")}}/images/favicon.png" type="image/x-icon">
-
+    <link rel="icon" href="{{ asset('assets') }}/images/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('assets') }}/images/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
     <title> @yield('title')</title>
 
     <!-- Google font-->
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+        rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap"
+        rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+        rel="stylesheet">
     <!-- Font Awesome-->
-    <link rel="stylesheet" type="text/css" href="{{asset("assets")}}/css/fontawesome.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/fontawesome.css">
     <!-- ico-font-->
-    <link rel="stylesheet" type="text/css" href="{{asset("assets")}}/css/icofont.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/icofont.css">
     <!-- Themify icon-->
-    <link rel="stylesheet" type="text/css" href="{{asset("assets")}}/css/themify.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/themify.css">
     <!-- Flag icon-->
-    <link rel="stylesheet" type="text/css" href="{{asset("assets")}}/css/flag-icon.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/flag-icon.css">
     <!-- Feather icon-->
-    <link rel="stylesheet" type="text/css" href="{{asset("assets")}}/css/feather-icon.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/feather-icon.css">
 
     <!-- Plugins css start-->
-    <link rel="stylesheet" type="text/css" href="{{asset("assets")}}/css/datatables.css">
-    <link rel="stylesheet" type="text/css" href="{{asset("assets")}}/css/sweetalert2.css">
-    <link rel="stylesheet" type="text/css" href="{{asset("assets")}}/css/select2.css">
-    <link rel="stylesheet" type="text/css" href="{{asset("assets")}}/css/date-picker.css">
-    <link rel="stylesheet" type="text/css" href="{{asset("assets")}}/css/daterange-picker.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/datatables.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/sweetalert2.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/select2.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/date-picker.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/daterange-picker.css">
     <!-- Plugins css Ends-->
 
     <!-- Bootstrap css-->
-    <link rel="stylesheet" type="text/css" href="{{asset("assets")}}/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/bootstrap.css">
     <!-- App css-->
-    <link rel="stylesheet" type="text/css" href="{{asset("assets")}}/css/style.css">
-    <link id="color" rel="stylesheet" href="{{asset("assets")}}/css/color-1.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/style.css">
+    <link id="color" rel="stylesheet" href="{{ asset('assets') }}/css/color-1.css" media="screen">
     <!-- Responsive css-->
-    <link rel="stylesheet" type="text/css" href="{{asset("assets")}}/css/responsive.css">
-    <link rel="stylesheet"  href="{{asset("css/custom.css")}}" />
-  </head>
-  <!-- <body class="dark-only"> -->
-    <body class="dark-sidebar">
-  <!-- <body> -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/responsive.css">
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}" />
+</head>
+<!-- <body class="dark-only"> -->
 
-  @php
-      $sessions = getSession();
-  @endphp
-  
+<body class="dark-sidebar">
+    <!-- <body> -->
+
+    @php
+        $sessions = getSession();
+    @endphp
+
     <!-- Loader starts-->
     {{-- <div class="loader-wrapper">
       <div class="theme-loader">    
@@ -65,13 +74,13 @@
     <!-- Loader ends-->
     <!-- page-wrapper Start-->
     <div class="page-wrapper" id="pageWrapper">
-      
+
         <!-- Page Header Start-->
         @include('admin.layout.header')
         <!-- Page Header Ends-->
 
-      <!-- Page Body Start-->
-      <div class="page-body-wrapper horizontal-menu">
+        <!-- Page Body Start-->
+        <div class="page-body-wrapper horizontal-menu">
 
             <!-- Page Sidebar Start-->
             @include('admin.layout.menubar')
@@ -85,46 +94,47 @@
             @include('admin.layout.footer')
             <!-- footer end-->
 
-      </div>
+        </div>
     </div>
     <!-- latest jquery-->
-    <script src="{{asset("assets")}}/js/jquery-3.5.1.min.js"></script>
+    <script src="{{ asset('assets') }}/js/jquery-3.5.1.min.js"></script>
     <!-- feather icon js-->
-    <script src="{{asset("assets")}}/js/icons/feather-icon/feather.min.js"></script>
-    <script src="{{asset("assets")}}/js/icons/feather-icon/feather-icon.js"></script>
+    <script src="{{ asset('assets') }}/js/icons/feather-icon/feather.min.js"></script>
+    <script src="{{ asset('assets') }}/js/icons/feather-icon/feather-icon.js"></script>
     <!-- Sidebar jquery-->
-    <script src="{{asset("assets")}}/js/sidebar-menu.js"></script>
-    <script src="{{asset("assets")}}/js/config.js"></script>
+    <script src="{{ asset('assets') }}/js/sidebar-menu.js"></script>
+    <script src="{{ asset('assets') }}/js/config.js"></script>
     <!-- Bootstrap js-->
-    <script src="{{asset("assets")}}/js/bootstrap/popper.min.js"></script>
-    <script src="{{asset("assets")}}/js/bootstrap/bootstrap.min.js"></script>
-    <!-- Plugins JS start-->   
-    <script src="{{asset("assets")}}/js/chart/apex-chart/apex-chart.js"></script>
-    <script src="{{asset("assets")}}/js/chart/apex-chart/stock-prices.js"></script>
-    <!-- <script src="{{asset("assets")}}/js/chart/apex-chart/chart-custom.js"></script> -->
+    <script src="{{ asset('assets') }}/js/bootstrap/popper.min.js"></script>
+    <script src="{{ asset('assets') }}/js/bootstrap/bootstrap.min.js"></script>
+    <!-- Plugins JS start-->
+    <script src="{{ asset('assets') }}/js/chart/apex-chart/apex-chart.js"></script>
+    <script src="{{ asset('assets') }}/js/chart/apex-chart/stock-prices.js"></script>
+    <!-- <script src="{{ asset('assets') }}/js/chart/apex-chart/chart-custom.js"></script> -->
 
-    <script src="{{asset("assets")}}/js/chart/google/google-chart-loader.js"></script>
-    <script src="{{asset("assets")}}/js/chart/google/google-chart.js"></script>
+    <script src="{{ asset('assets') }}/js/chart/google/google-chart-loader.js"></script>
+    <script src="{{ asset('assets') }}/js/chart/google/google-chart.js"></script>
 
-    <script src="{{asset("assets")}}/js/datatable/datatables/jquery.dataTables.min.js"></script>
-    <script src="{{asset("assets")}}/js/datatable/datatables/datatable.custom.js"></script>
-    <script src="{{asset("assets")}}/js/tooltip-init.js"></script>
+    <script src="{{ asset('assets') }}/js/datatable/datatables/jquery.dataTables.min.js"></script>
+    <script src="{{ asset('assets') }}/js/datatable/datatables/datatable.custom.js"></script>
+    <script src="{{ asset('assets') }}/js/tooltip-init.js"></script>
 
-    <script src="{{asset("assets")}}/js/sweet-alert/sweetalert.min.js"></script>
-    <script src="{{asset("assets")}}/js/select2/select2.full.min.js"></script>
-    <script src="{{asset("assets")}}/js/select2/select2-custom.js"></script>
+    <script src="{{ asset('assets') }}/js/sweet-alert/sweetalert.min.js"></script>
+    <script src="{{ asset('assets') }}/js/select2/select2.full.min.js"></script>
+    <script src="{{ asset('assets') }}/js/select2/select2-custom.js"></script>
 
-    <script src="{{asset("assets")}}/js/datepicker/daterange-picker/moment.min.js"></script>
-    <script src="{{asset("assets")}}/js/datepicker/daterange-picker/daterangepicker.js"></script>
-    <script src="{{asset("assets")}}/js/datepicker/daterange-picker/daterange-picker.custom.js"></script>
+    <script src="{{ asset('assets') }}/js/datepicker/daterange-picker/moment.min.js"></script>
+    <script src="{{ asset('assets') }}/js/datepicker/daterange-picker/daterangepicker.js"></script>
+    <script src="{{ asset('assets') }}/js/datepicker/daterange-picker/daterange-picker.custom.js"></script>
     <!-- Plugins JS Ends-->
     <!-- Theme js-->
-    <script src="{{asset("assets")}}/js/script.js"></script>
+    <script src="{{ asset('assets') }}/js/script.js"></script>
     {{-- <!-- <script src="{{asset("assets")}}/js/theme-customizer/customizer.js"></script> --> --}}
     <!-- login js-->
     <!-- Plugin used-->
 
     @yield('js')
 
-  </body>
+</body>
+
 </html>
